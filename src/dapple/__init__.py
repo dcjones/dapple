e9d6e80596013b2d3e8e6b4f18184ec370a5de31
Daniel C. Jones <djones3@fredhutch.org>
2025-10-16 15:32:45 -0700

    Import a few more things into the top level.

--- src/dapple/__init__.py
+++ src/dapple/__init__.py
@@ -1,3 +1,11 @@
 from .plot import plot
 from .colors import color
 from .coordinates import mm, cm, pt, inch, cx, cxv, cy, cyv, vw, vwv, vh, vhv
+from .scales import (
+    ycontinuous,
+    ydiscrete,
+    xcontinuous,
+    xdiscrete,
+    colorcontinuous,
+    colordiscrete,
+)
